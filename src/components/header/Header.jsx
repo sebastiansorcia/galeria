911eca062cbbabfaf482d062de43fb96d0906db6
sebastian <sebastianseberianodev@gmail.com>
2023-09-28 10:37:12 -0600

    changes ui

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,11 +4,16 @@ function Header() {
     return (
         <header className="header__images__app">
             <nav className="header__navbar__app container-mx">
-                <div className="header__title">
-                    <h1 className="title">Gallery</h1>
+                <div className="item-component">
+                    <div className="header__title">
+                        <h1 className="title">Gallery</h1>
+                    </div>
+                    <div className="generated__for">
+                        <span className="text">developed by sebastian</span>
+                    </div>
                 </div>
-                <div className="generated__for">
-                    <span className="text">developed by sebastian</span>
+                <div className="switch-theme">
+                    <label htmlFor="darkmode" className="switch"></label>
                 </div>
             </nav>
         </header>
